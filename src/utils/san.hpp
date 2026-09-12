580f079fa53debcc53b8cc86e4834e3e100d8045
will <93092420+vapide@users.noreply.github.com>
2026-09-12 17:54:40 -0400

    added san skeleton

---
+++ src/utils/san.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+#include "../core/move.hpp"
+
+class Position;
+
+// algebriac notation for a legal move in pos
+// check/mate suffix detection uses make and uses unmake, making Position non-const
+std::string move_to_san(Position& pos, Move move);
