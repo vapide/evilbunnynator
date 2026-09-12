e173a239041a09cd870e832411005047065ef57c
will <93092420+vapide@users.noreply.github.com>
2026-09-12 13:53:42 -0400

    bitboard class and header, uses <bit>

---
+++ src/core/bitboard.cpp
@@ -0,0 +1,28 @@
+#include "bitboard.hpp"
+
+#include <iostream>
+
+
+namespace Bitboard {
+
+void pretty_print(U64 bb)
+{
+    for (int rank = 7; rank >= 0; --rank)
+    {
+        for (int file = 0; file < 8; ++file)
+        {
+            int square = rank * 8 + file;
+
+            if (bb & (1ULL << square))
+                std::cout << "1 ";
+            else
+                std::cout << ". ";
+        }
+
+        std::cout << '\n';
+    }
+
+    std::cout << '\n';
+}
+
+}
