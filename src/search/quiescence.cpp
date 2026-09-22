bfe1d7f496af0504380c18edffaba80302522128
will <93092420+vapide@users.noreply.github.com>
2026-09-22 15:39:32 -0400

    fixed quiescence
    Still a bit shady in terms of check moves, I have to think that one through.

--- src/search/quiescence.cpp
+++ src/search/quiescence.cpp
@@ -15,7 +15,9 @@ int Search::quiescence(Position& pos, int alpha, int beta, int ply) {
 
   ++nodes;
   ++qnodes;
+
   if (ply > seldepth) seldepth = ply;
+
   // same as negamax, but with a nuance-there is no depth counter that runs out,
   // it returns an eval instead of a mate or draw score, because the search ceil
   // is not making a claim, its more like giving up
@@ -23,12 +25,35 @@ int Search::quiescence(Position& pos, int alpha, int beta, int ply) {
 
   // likely to be the only initialization of best score that isnt -INF_SCORE
   // this is because node has score already, and stm may decline the capture.
-  best_score = evaluate(pos);
+  int best_score = evaluate(pos);
 
-  if (best_score >= beta) {
-    return best_score;
-  } else if (best_score > alpha) {
-    alpha = best_score;
+  if (best_score >= beta) return best_score;
+
+  if (best_score > alpha) alpha = best_score;
+
+  Move moves[MAX_MOVES];
+  const int count = MoveGen::generate_legal_captures(pos, moves);
+
+  for (int i = 0; i < count; ++i) {
+    do_move(pos, moves[i]);
+    const int score = -quiescence(pos, -beta, -alpha, ply + 1);
+    undo_move(pos);
+    if (stop.load(std::memory_order_relaxed)) {
+      // ok here because we already have a valid score, not -INF_SCORE;
+      return best_score;
+    }
+
+    if (score > best_score) {
+      best_score = score;
+    }
+
+    if (score > alpha) {
+      alpha = score;
+    }
+
+    if (score >= beta) {
+      return score;
+    }
   }
 
   return best_score;
